a49bb603eb48dd8e2b6cd0bcea7e64901ace82b7
Alan Santos <alansousa@outlook.com>
2026-06-17 02:16:31 +0000

    feat: add `tasks` creation route to `api` routes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
+    // Auth routes
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Tasks routes
+    Route::post('/tasks', [TaskController::class, 'store']);
 });
